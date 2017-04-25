bd95946425c33286e9fbe2984b814a306d84e033
Len <a78965@gmail.com>
2017-04-25 17:57:06 +0800

    Add bill and BillItem.vue

---
+++ js/components/BillItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="panel panel-default">
+    <!-- Default panel contents -->
+    <div class="panel-heading"><h4>帳單明細</h4></div>
+    <div class="panel-body">
+      <dl class="dl-horizontal">
+        <dt>帳單編號</dt>
+        <dd>{{ bill.id }}</dd>
+
+        <dt>使用者名稱</dt>
+        <dd>{{ bill.user_name }}</dd>
+
+        <dt>租屋地址</dt>
+        <dd>{{ bill.address }}</dd>
+
+        <dt>帳單費用總計</dt>
+        <dd>{{ bill.total }}</dd>
+
+        <dt>帳單計費開始時間</dt>
+        <dd>{{ bill.date_start }}</dd>
+
+        <dt>帳單計費結束時間</dt>
+        <dd>{{ bill.date_end }}</dd>
+
+        <dt>繳費截止時間</dt>
+        <dd>{{ bill.expire }}</dd>
+
+        <dt>備註</dt>
+        <dd><blockquote>{{ bill.note }}</blockquote></dd>
+      </dl>
+    </div>
+    <div class="panel-footer">
+      <button :data-link="bill_link + '/' + bill.id" class="btn btn-success">帳單明細</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['bill', 'bill_link'],
+}
+</script>
---
+++ js/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+
+Vue.component('bill-item', require('./components/BillItem.vue'))
+
+new Vue({
+    el: '#bills',
+    data: {
+        bill_link: "https://localhost",
+        bills: null,
+    },
+    methods: {
+        showBills: function(bill_id) {
+            var self = this;
+            $.get("data/" + bill_id + '.json', function(data) {
+                self.bills = data;
+            });
+        }
+    }
+});
